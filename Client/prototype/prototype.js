a7fe6c418304d860d501d109692eb8e8c42b4314
Leo Sadoun <Leosadoun4@gmail.com>
2024-11-30 22:17:12 -0500

    added log in button popup & user dropdown

--- Client/prototype/prototype.js
+++ Client/prototype/prototype.js
@@ -1,13 +1,13 @@
 // swiper 
-// var swiper = new Swiper(".home", {
-//     spaceBetween: 30,
-//     centeredSlides: true,
+var swiper = new Swiper(".home", {
+    spaceBetween: 30,
+    centeredSlides: true,
    
-//     navigation: {
-//       nextEl: ".swiper-button-next",
-//       prevEl: ".swiper-button-prev",
-//     },
-//   });
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
 
   let menu = document.querySelector('#menu-icon');
   let navbar = document.querySelector('.navbar');
@@ -17,8 +17,46 @@
     navbar.classList.toggle('active');
       
   }
-  // window.onscroll = () => {
-  //   menu.classList.remove('bx-x');
-  //   navbar.classList.remove('active');
-  // }
+  window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+  }
   
+
+//Dropdown
+const profile = document.getElementById('profile');
+const dropdown = document.getElementById('dropdown');
+
+profile.addEventListener('click', () => {
+    dropdown.style.display = dropdown.style.display === 'block' ? 'none' : 'block';
+});
+
+// Close dropdown if clicked outside
+document.addEventListener('click', (event) => {
+    if (!profile.contains(event.target)) {
+        dropdown.style.display = 'none';
+    }
+});
+
+
+// JavaScript to toggle popup
+const loginBtn = document.getElementById('loginBtn');
+const popup = document.getElementById('popup');
+const closeBtn = document.getElementById('closeBtn');
+
+// Show popup
+loginBtn.addEventListener('click', () => {
+    popup.style.display = 'block';
+});
+
+// Close popup
+closeBtn.addEventListener('click', () => {
+    popup.style.display = 'none';
+});
+
+// Close popup if clicked outside
+window.addEventListener('click', (e) => {
+  if (e.target === popup) {
+      popup.style.display = 'none'; // Only hide the popup if clicked outside
+  }
+});
